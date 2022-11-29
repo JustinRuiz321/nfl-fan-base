f8d24e3f34b8fc725f1184368722e54797fa03b4
DylanSantiago <2dylansantiago@gmail.com>
2022-11-28 21:22:01 -0500

    users seeded

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,5 +1,9 @@
 const db = require('./connection');
-const { User } = require('../models');
-const { Comment } = require('../models');
+const { User, Comment } = require('../models');
 
-
+await User.create({
+    username: 'user100',
+    email: 'user100@gmail.com',
+    password: 'password1',
+    comments: 'Mike White looks like the real deal'
+})
