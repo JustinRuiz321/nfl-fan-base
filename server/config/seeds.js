021d332ce067baf7b98dedb56c4d66c78fe5f409
DylanSantiago <2dylansantiago@gmail.com>
2022-11-28 21:03:54 -0500

    comment model added

---
+++ server/config/seeds.js
@@ -0,0 +1,5 @@
+const db = require('./connection');
+const { User } = require('../models');
+const { Comment } = require('../models');
+
+
---
+++ server/models/comment.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+const commentSchema = new Schema({
+
+    commentText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: new Date ()
+    },
+    username: {
+        type: String,
+        required: true
+    },
+        
+    reactions: [reaction],
+    },
+);
+// thoughtSchema.virtual('reactionCount').get(function () {
+//     return this.reactions.length
+// });
+const Comment = model('Comment', commentSchema);
+module.export = Comment;
