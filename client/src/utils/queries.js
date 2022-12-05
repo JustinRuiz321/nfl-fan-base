5129bdcbe66e4385281fc888adab728d4baf7310
DylanSantiago <2dylansantiago@gmail.com>
2022-12-05 18:06:03 -0500

    queries.js file updated to retrieve info on graphql

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,48 @@
-export const ADD_COMMENT = gql`
-  mutation addComment($commentText: String!) {
-    addComment(commentText: $commentText) {
+export const QUERY_COMMENTS = gql`
+  query getComments {
+    comments {
+      _id
+      commentText
+      commentAuthor
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_SINGLE_COMMENT = gql`
+  query getSingleComment($commentId: ID!) {
+    comment(commentId: $commentId) {
       _id
       commentText
       commentAuthor
